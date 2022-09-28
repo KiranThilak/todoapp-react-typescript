c23c3e905ae21bcb539010f1be137bf4d84cc511
kiranthilak <kiran.thilak@yahoo.com>
2022-09-28 13:27:42 +0530

    unicode tick icon added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import InputFeild from './components/InputFeild';
 const App: React.FC = () => {
   return (
     <div className='App'>
-  <span className="heading">Taskify</span>
+  <span className="heading">	
+&#9989; Taskify</span>
   <InputFeild />
 
     
